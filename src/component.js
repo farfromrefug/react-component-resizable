a3fa6634d9d64a67e3d566a3fa19b1d47a8afa94
Alexey Zinoviev <alexey.zinoviev@xored.com>
2016-08-31 19:15:59 +0700

    #5 opacity 0 added to resize-triggers style

--- src/component.js
+++ src/component.js
@@ -98,7 +98,7 @@ var Resizable = React.createClass({
               React.createElement('div', {className: contractClass, ref: 'contract', key: 'contract'})
             ]
           ),
-          embedCss ? React.createElement('style', {key: 'embededCss', dangerouslySetInnerHTML: {__html: '.resize-triggers { visibility: hidden; } .resize-triggers, .resize-triggers > div, .contract-trigger:before { content: \" \"; display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; } .resize-triggers > div { background: #eee; overflow: auto; } .contract-trigger:before { width: 200%; height: 200%; }'}}) : null
+          embedCss ? React.createElement('style', {key: 'embededCss', dangerouslySetInnerHTML: {__html: '.resize-triggers { visibility: hidden; opacity: 0; } .resize-triggers, .resize-triggers > div, .contract-trigger:before { content: \" \"; display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; } .resize-triggers > div { background: #eee; overflow: auto; } .contract-trigger:before { width: 200%; height: 200%; }'}}) : null
         ]
       )
     );
